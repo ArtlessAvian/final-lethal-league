4985721acfe26d214ed25637ce3d8f963036e387
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2017-05-01 11:06:45 -0700

    Setup LibGDX

---
+++ desktop/src/com/artlessavian/lethalleague/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.artlessavian.lethalleague.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.artlessavian.lethalleague.Maineroni;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Maineroni(), config);
+	}
+}

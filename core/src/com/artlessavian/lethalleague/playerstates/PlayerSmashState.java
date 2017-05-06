78bbbd803335f66d1308955645716a738f5c443e
ArtlessAvian <ArtlessAvian@gmail.com>
2017-05-06 05:27:59 -0700

    Add more empty classes

---
+++ core/src/com/artlessavian/lethalleague/playerstates/PlayerSmashState.java
@@ -0,0 +1,51 @@
+package com.artlessavian.lethalleague.playerstates;
+
+import com.artlessavian.lethalleague.State;
+import com.artlessavian.lethalleague.StateMachine;
+import com.artlessavian.lethalleague.ecs.components.PhysicsComponent;
+import com.artlessavian.lethalleague.ecs.entities.Player;
+
+public class PlayerSmashState extends State
+{
+	Player player;
+
+	public PlayerSmashState(Player player)
+	{
+		this.player = player;
+	}
+
+	@Override
+	public void exit()
+	{
+		// TODO
+
+	}
+
+	@Override
+	public void enter()
+	{
+		// TODO
+
+	}
+
+	@Override
+	public boolean changeStateMaybe(StateMachine sm)
+	{
+		// TODO
+		return false;
+	}
+
+	@Override
+	public void doStuff()
+	{
+		PhysicsComponent physicsC = player.getComponent(PhysicsComponent.class);
+
+		// TODO
+	}
+
+	@Override
+	public int getSpriteID()
+	{
+		return 0;
+	}
+}
